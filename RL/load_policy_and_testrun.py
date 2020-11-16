ebe639e4907b07b0d57fa73516450c7e4c82af89
Andreas-aha <andreas.haege@ipg-automotive.com>
2020-11-16 12:45:50 +0100

    Fixing...

---
+++ RL/load_policy_and_testrun.py
@@ -0,0 +1,44 @@
+import tensorflow as tf
+import numpy as np
+import os
+
+from tf_agents.environments import py_environment
+from tf_agents.environments import tf_environment
+from tf_agents.environments import tf_py_environment
+from tf_agents.environments import utils
+from tf_agents.specs import array_spec
+from tf_agents.environments import wrappers
+from tf_agents.trajectories import time_step as ts
+
+from tf_agents.agents.ddpg import critic_network
+from tf_agents.agents.sac import sac_agent
+from tf_agents.agents.sac import tanh_normal_projection_network
+from tf_agents.environments import suite_pybullet
+from tf_agents.experimental.train import actor
+from tf_agents.experimental.train import learner
+from tf_agents.experimental.train import triggers
+from tf_agents.experimental.train.utils import spec_utils
+from tf_agents.experimental.train.utils import strategy_utils
+from tf_agents.experimental.train.utils import train_utils
+from tf_agents.metrics import py_metrics
+from tf_agents.networks import actor_distribution_network
+from tf_agents.policies import greedy_policy
+from tf_agents.policies import py_tf_eager_policy
+from tf_agents.policies import random_py_policy
+from tf_agents.replay_buffers import reverb_replay_buffer
+from tf_agents.replay_buffers import reverb_utils
+
+from tf_agents.utils import common as common_utils
+
+common_utils.train_checkpointer.initialize_or_restore()
+global_step = tf.compat.v1.train.get_global_step()
+
+
+def run_episodes_and_create_video(policy, eval_tf_env, eval_py_env):
+  num_episodes = 5
+  for _ in range(num_episodes):
+    time_step = eval_tf_env.reset()
+    while not time_step.is_last():
+      action_step = policy.action(time_step)
+      time_step = eval_tf_env.step(action_step.action)
+
