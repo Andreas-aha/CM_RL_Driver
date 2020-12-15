43e7e7e7699ae94f72f90f692dddb8d1611d5fa9
Andreas-aha <andreas.haege@ipg-automotive.com>
2020-12-15 18:11:23 +0100

    Added proc for ovrwrite of rtfac set by python

--- .CarMaker.tcl
+++ .CarMaker.tcl
@@ -1,8 +1,14 @@
-set Pgm(TcpCmdPort) 14100
+#set Pgm(TcpCmdPort) 14100
 
 
 proc Load_Autostart_TestRun {} {
 	after idle {source Data/Script/Autostart_TestRun.tcl}
 }
 
+proc Load_Reset_RTFac {} {
+	after idle {source Data/Script/Reset_RTFac.tcl}
+}
+
 Load_Autostart_TestRun
+Load_Reset_RTFac
+
